b35e54708a92adc7efcb6e80ad29850c4478eef5
Pramod Thomas <pmod23@gmail.com>
2020-05-22 18:30:38 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -8,5 +8,5 @@ def monopoly_with_third_tier
   {:railroads=>{:pieces=>4, :rent_in_dollars=>{:one_piece_owned=>25, :two_pieces_owned=> 50, :three_pieces_owned=>100, :four_pieces_owned=>200}},:names=>{:reading_railraod=>{}, :pennsylvania_railroad=>{}, :b_and_o_railroad=>{}, :shortline_railroad=>{}} 
 end
 def monopoly_with_fourth_tier
-{:railroads=>{:pieces=>4, :rent_in_dollars=>{:one_piece_owned=>25, :two_pieces_owned=> 50, :three_pieces_owned=>100, :four_pieces_owned=>200}},:names=>{:reading_railraod=>{100}, :pennsylvania_railroad=>{200}, :b_and_o_railroad=>{400}, :shortline_railroad=>{}} 
+{:railroads=>{:pieces=>4, :rent_in_dollars=>{:one_piece_owned=>25, :two_pieces_owned=> 50, :three_pieces_owned=>100, :four_pieces_owned=>200}},:names=>{:reading_railraod=>{100}, :pennsylvania_railroad=>{200}, :b_and_o_railroad=>{400}, :shortline_railroad=>{800}} 
 end
